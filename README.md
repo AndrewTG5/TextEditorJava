d2b816dec6d9dd3209ea2edf40d1ee236942822d
andrewthegreat5 <andy123llk@gmail.com>
2022-08-09 15:07:30 +1200

    build out a UI and some misc changes

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # 251-Assignment1-2022-Andrew-Rachel
+![Build Status](https://github.com/andrewthegreat5/251-Assignment1-2022-Andrew-Rachel/.github/workflows/CI.yml/badge.svg)
+
 Andrew Blake - 21009078
 
 
---
+++ src/main/java/com/example/texteditor/MainController.java
@@ -0,0 +1,36 @@
+package com.example.texteditor;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class MainController {
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private void onNew(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void onOpen(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void onSave(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void onSaveAs(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void onExit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+}
