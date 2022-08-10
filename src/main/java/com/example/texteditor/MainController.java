499f1e68439d3b96c9d81e1fe32b1d1c6d8186c9
andrewthegreat5 <andy123llk@gmail.com>
2022-08-10 12:03:55 +1200

    small changes

--- src/main/java/com/example/texteditor/MainController.java
+++ src/main/java/com/example/texteditor/MainController.java
@@ -1,35 +1,28 @@
 package com.example.texteditor;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
 public class MainController {
 
-    @FXML
-    private TextArea textArea;
+    public TextArea textArea;
 
-    @FXML
-    private void onNew(ActionEvent actionEvent) {
+    public void onNew(ActionEvent actionEvent) {
 
     }
 
-    @FXML
     public void onOpen(ActionEvent actionEvent) {
 
     }
 
-    @FXML
     public void onSave(ActionEvent actionEvent) {
 
     }
 
-    @FXML
     public void onSaveAs(ActionEvent actionEvent) {
 
     }
 
-    @FXML
     public void onExit(ActionEvent actionEvent) {
         System.exit(0);
     }
