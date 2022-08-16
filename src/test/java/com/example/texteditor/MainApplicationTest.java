76d80e695eea76fc5187cb9ade3c2a147ee2e1bf
andrewthegreat5 <andy123llk@gmail.com>
2022-08-16 12:36:42 +1200

    on new file or windows close, prompt the user to save file if there are changes. controller object now available in application.

--- src/test/java/com/example/texteditor/MainApplicationTest.java
+++ src/test/java/com/example/texteditor/MainApplicationTest.java
@@ -18,4 +18,38 @@ class MainApplicationTest {
         assertEquals(content, MainApplication.readFile(new File("src/test/resources/test.txt")));
     }
 
+    @Test
+    @DisplayName("Test MainApplication.isUnsaved() with test.txt and modified content")
+    void testIsUnsaved() {
+        String content = "This is an example test file." + System.lineSeparator() +
+                System.lineSeparator() +
+                "There is a blank line above this one!f" + System.lineSeparator();
+        assertTrue(MainApplication.isUnsaved(new File("src/test/resources/test.txt"), content));
+    }
+
+    @Test
+    @DisplayName("Test MainApplication.isUnsaved() with null and content")
+    void testIsUnsavedNull() {
+        String content = "This is an example test file." + System.lineSeparator() +
+                System.lineSeparator() +
+                "There is a blank line above this one!f" + System.lineSeparator();
+        assertTrue(MainApplication.isUnsaved(null, content));
+    }
+
+    @Test
+    @DisplayName("Test false MainApplication.isUnsaved() with null and blank content")
+    void testIsUnsavedNew() {
+        String content = "";
+        assertFalse(MainApplication.isUnsaved(null, content));
+    }
+
+    @Test
+    @DisplayName("Test false MainApplication.isUnsaved() with test.txt and content")
+    void testIsUnsavedFalse() {
+        String content = "This is an example test file." + System.lineSeparator() +
+                System.lineSeparator() +
+                "There is a blank line above this one!" + System.lineSeparator();
+        assertFalse(MainApplication.isUnsaved(new File("src/test/resources/test.txt"), content));
+    }
+
 }
